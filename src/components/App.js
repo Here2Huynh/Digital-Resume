da0741a71667c12b1b131797bbc35f928d47c44d
John Huynh <jhnhnyh9@gmail.com>
2022-09-11 00:24:44 -0700

    wired up tailwindcss

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>resume</div>;
+  return (
+    <div className="mockup-window border bg-base-300">
+      <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import "./app.css";
 
 createRoot(document.querySelector("#root")).render(<App />);
